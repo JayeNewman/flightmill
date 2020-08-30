2964fc8d23fc98308ae907980a0f814ef376e0b0
Jaye Newman <jayenewman85@gmail.com>
2020-08-30 12:32:51 +1000

    The summary data can now be bound with the other chamber data for comparison and analysis.

--- R/fm_summary.R
+++ R/fm_summary.R
@@ -4,16 +4,23 @@
 #' @param df Data frame to be modified. Use the data frame created from bouts and rest_dration functions.
 #' @param bout_summary_data Data frame of the bouts function.
 #' @param bout Vector created from the bouts function.
-#' @param mean_speed_bout Vector created from the bout data frame
+#' @param mean_speed_bout Vector created from the fm_speed data frame
 #' @return A data frame with the bout flight parameters for the specified flight mill chamber
 #' @export
 
-fm_summary <- function(df, bout_summary_data, bout, mean_speed_bout) {
-  df <- df %>%
-    dplyr::mutate(max_speed_cms = max(speedCMS),
-                  mean_speed_cms = mean(speedCMS),
-                  median_speed_cms = median(speedCMS),
-                  distance_km = max(distance_cm/100000),
-                  num_bouts = sum(bout_summary_data$bout == "TRUE")) %>%
-    dplyr::mutate(ttl_mean_bout_speedcms = mean(bout_summary_data$mean_speed_bout_cms))
+fm_summary <- function(bout_data, ch_data, ch_dr, ch_mean_speed, fm_total_duration) {
+  bout_data <- bout_data %>%
+    dplyr::mutate(max_speed_cms = max(ch_data$speedCMS),
+           total_mean_speed_cms = ch_mean_speed$mean_cms_true_bouts,
+           distance_km = max(ch_data$distance_cm/100000),
+           num_bouts = sum(bout_data$bout == "TRUE"),
+           flight_duration = ch_dr$total_flight_duration_sec,
+           rest_duration = fm_total_duration - flight_duration) %>%
+    dplyr::group_by(bout) %>%
+    base::subset(!bout %in% unique(bout[bout == FALSE])) %>%
+    dplyr::mutate(run = first(ch_data$run),
+           chamber = first(ch_data$chamber),
+           id = first(ch_data$id),
+           species = first(ch_data$species))
+  return(bout_data)
 }
