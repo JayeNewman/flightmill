686d090520b7b1ba167b41f2cd9602eb74cf6481
Jaye Newman <jayenewman85@gmail.com>
2020-10-08 07:50:14 +1000

    File lookup specified outside the function
    The user will create two lookup lists that will be combined in the function as a lookup table which matches the chamber column values to the id values.

--- R/fm_input.R
+++ R/fm_input.R
@@ -3,38 +3,66 @@
 #' @name fm_input
 #' @title Importing multiple files for a flight mill run
 #' @description Loads multiple files from a single directory. The files are expected to have a first cell that has the flight mill run information. This information is extracted from a selected file and new columns are created with the time information which is parsed to a POSIX format.The files are combined and tidied and extra information on species and chamber for the eight chambers id is required.
-#' @param file_dir locates the specified directory
-#' @param file_name choose a file in the directory that has the run values on the first line
-#' @param sp_name the name of the species used in the mill. Expects only one species
-#' @param id1, id2, to 1d8, is the chamber code id's of the insect that were flown for each chamber
-#' @return A data frame of all the files within the directory. They will be combined
+#' @param file_dir locates the specified directory.
+#' @param file_name choose a file in the directory that has the run values on the first line.
+#' @param sp_name the name of the species used in the mill. Expects only one species.
+#' @return A combined data frame of all the files within the directory.
 #' @export
 
-fm_input <- function(file_dir, file_name, sp_name, id1, id2, id3, id4, id5, id6, id7, id8) {
-  csv_files <- fs::dir_ls(file_dir)
-  txt_row <- utils::read.csv(file_name,
-                             header = FALSE, nrows = 1) %>%
+# To import the id_lookup dataframe with two columns: id and chamber which contains the unique pairings
+# User will need to create a lookup table to be able to use this
+
+# An example lookup table:
+#
+# dir_run <- ("/data/20091703")
+#
+# chamber <- c("CHAMBER1", "CHAMBER2", "CHAMBER3", "CHAMBER4",
+#              "CHAMBER5", "CHAMBER6", "CHAMBER7", "CHAMBER8")
+#
+# id <- c("id1", "id2", "id3", "id4",
+#         "id5", "id6", "id7", "id8")
+
+fm_input <- function(file_dir, file_name, sp_name) {
+
+  csv_files <- fs::dir_ls(paste0(getwd(),dir_run))
+
+  csv_files <- csv_files %>%
+    purrr::map_dfr(read_csv, skip = 1, .id = "chamber")
+
+  txt_row <- utils::read.csv(file_name, header = FALSE, nrows = 1) %>%
     stringr::str_trunc(width = 20, side = 'left', ellipsis = '')
 
   csv_files <- csv_files %>%
     purrr::map_dfr(read_csv, skip = 1, .id = "chamber")
-  files <- csv_files %>%
+
+  df <- csv_files %>%
     dplyr::mutate_at("chamber", str_trunc, width = 21, side = 'left', ellipsis = '') %>%
     tidyr::separate("chamber", c('run', 'chamber', '/')) %>%
     dplyr::select(-"/") %>%
     janitor::clean_names("snake") %>%
     dplyr::mutate(starting_time = rep(dmy_hms(txt_row)),
-                  across(where(is_character), as_factor),
                   time_series = starting_time + elapsed_sec,
-                  species = sp_name,
-                  id = case_when(chamber == "CHAMBER1" ~ id1,
-                                 chamber == "CHAMBER2" ~ id2,
-                                 chamber == "CHAMBER3" ~ id3,
-                                 chamber == "CHAMBER4" ~ id4,
-                                 chamber == "CHAMBER5" ~ id5,
-                                 chamber == "CHAMBER6" ~ id6,
-                                 chamber == "CHAMBER7" ~ id7,
-                                 chamber == "CHAMBER8" ~ id8
-                  ))
-  return(files)
+                  species = sp_name)
+
+  id_lookup <- data.frame(chamber, id, stringsAsFactors = FALSE)
+
+  n <- nrow(df)
+
+  df <- cbind(df, id = rep(NA, n))
+
+  #id_lookup dataframe with two columns: id and chamber which contains the unique pairings
+
+
+  for(i in 1:nrow(id_lookup))
+  {
+    rowInds <- which(df[, "chamber"] == id_lookup[i, "chamber"])
+    if(length(rowInds) == 0)
+    {
+      #stop("One of the chambers in the lookup does not have any corresponding rows in the dataframe.")
+      cat("Warning.... \n")
+    }
+    df[rowInds, "id"] <- id_lookup[i, "id"]
+  }
+
+  return(df)
 }
