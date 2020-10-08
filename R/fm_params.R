dedd545709b0da2579cf47761610209a33c64d45
Jaye Newman <jayenewman85@gmail.com>
2020-10-08 11:13:23 +1000

    remove any value < 0 in the counter ticker
    Removing these values takes out the abnormal data. Making it much cleaner.

--- R/fm_params.R
+++ R/fm_params.R
@@ -6,26 +6,58 @@
 #' @param chamber Parameter to be grouped for analysis.
 #' @param elapsed_sec Vector of the elapsed seconds parameter.
 #' @param counter Vector of the count parameter.
+#' @param sector_distance The part of the circumference that the insect has traveled between each sensor break.
 #' @param lowest_speed Lowest acceptable speed for a bout to become FALSE.
 #' @param min_time_interval Shortest time interval allowable between infrared breaks. This is to omit unrealistic speeds. The usual acceptable level is 0.01
 #' @param fm_duration The length of time in seconds of the flight mill run
 #' @return A data frame with flight parameters calculated from elapsed seconds and count grouped by the chambers of the flight mill.
 #' @export
 
-fm_params <- function(df, chamber, elapsed_sec, counter, lowest_speed, min_time_interval, fm_duration) {
+fm_params <- function(df, chamber, elapsed_sec, counter, sector_distance, lowest_speed, min_time_interval, fm_duration)
+  {
   df <- df %>%
     dplyr::group_by(chamber) %>%
+    dplyr::mutate(counter_ticker = counter - lag(counter, default = counter[1]),
+                  radius_at_break = ifelse(counter_ticker == "0.1" | counter_ticker >= "0.0", sector_distance, -sector_distance),
+                  counter_ticks = replace(counter_ticker, which(counter_ticker < 0), NA)) %>%
+    na.omit() %>%
     dplyr::mutate(time_interval = elapsed_sec - lag(elapsed_sec, default = elapsed_sec[1]),
-                  counter_ticker = counter - lag(counter, default = counter[1]),
-                  radius_at_break = ifelse(counter_ticker == "0.1" | counter_ticker >= "0.0", 9, -9),
-                  speedCMS = radius_at_break / time_interval,
+                  speed_cms = radius_at_break / time_interval,
+                  speed_cms = replace(speed_cms, which(speed_cms < 0), 0),
                   speed_kmph = speedCMS * 0.036,
+                  speed_kmph = replace(speed_kmph, which(speed_kmph < 0), 0),
                   distance_cm = cumsum(radius_at_break),
                   distance_m = distance_cm * 0.01,
                   bout = case_when(speedCMS < lowest_speed ~FALSE, speedCMS >= lowest_speed ~TRUE, TRUE ~NA)) %>%
     dplyr::filter(!duplicated(elapsed_sec),
                   time_interval > min_time_interval,
-                  elapsed_sec < fm_duration) %>%
-    dplyr::select(-counter_ticker);
+                  elapsed_sec < fm_duration)
+  df <- df %>%
+    group_by(chamber) %>%
+    slice(3:n())
+
   return(df)
 }
+
+
+# flight5 <- run_name %>%
+#   dplyr::group_by(chamber) %>%
+#   dplyr::mutate(counter_ticker = counter - lag(counter, default = counter[1]),
+#                 radius_at_break = ifelse(counter_ticker == "0.1" | counter_ticker >= "0.0", 9, -9),
+#                 counter_ticks = replace(counter_ticker, which(counter_ticker<0), NA)) %>%
+#   na.omit() %>%
+#   mutate(time_interval = elapsed_sec - lag(elapsed_sec, default = elapsed_sec[1]),
+#          speedCMS = radius_at_break / time_interval,
+#          speed_kmph = speedCMS * 0.036,
+#          speedCMS = replace(speedCMS, which(speedCMS<0), 0),
+#          speed_kmph = replace(speed_kmph, which(speed_kmph<0), 0),
+#          distance_cm = cumsum(radius_at_break),
+#          distance_m = distance_cm * 0.01,
+#          bout = case_when(speedCMS < 20 ~FALSE, speedCMS >= 20 ~TRUE, TRUE ~NA)) %>%
+#   dplyr::filter(!duplicated(elapsed_sec),
+#                 time_interval > 0.05,
+#                 elapsed_sec < 3600)
+#
+# flight <- flight5 %>%
+#   group_by(chamber) %>%
+#   slice(3:n())
