33155768084c1baa7bd2d55408afcf39eac7127d
Jaye Newman <jayenewman85@gmail.com>
2020-11-25 19:29:00 +1000

    Fixed the renaming of chamber to mill

--- R/fm_input.R
+++ R/fm_input.R
@@ -61,8 +61,8 @@ fm_input <- function(file_dir, file_name, sp_name) {
     df[rowInds, "id"] <- id_lookup[i, "id"]
   }
 
-  # df <- df %>%
-  #   rename_with(mill = chamber)
+  df <- df %>%
+    rename(mill = chamber)
 
   return(df)
 }
